6c57e62e6f2aacce6173a61c39a2223c71521297
senethmendis <mendis.seneth@gmail.com>
2024-08-19 02:22:56 +0530

    game 2

---
+++ memorygame/app.js
@@ -0,0 +1,74 @@
+const cardArry = [
+  {
+    name: "fries",
+    img: "images/fries.png",
+  },
+  {
+    name: "cheeseburger",
+    img: "images/cheeseburger.png",
+  },
+
+  {
+    name: "hotdog",
+    img: "images/hotdog.png",
+  },
+  {
+    name: "ice-cream",
+    img: "images/ice-cream.png",
+  },
+  {
+    name: "milkshake",
+    img: "images/milkshake.png",
+  },
+  {
+    name: "pizza",
+    img: "images/pizza.png",
+  },
+
+  {
+    name: "fries",
+    img: "images/fries.png",
+  },
+  {
+    name: "cheeseburger",
+    img: "images/cheeseburger.png",
+  },
+
+  {
+    name: "hotdog",
+    img: "images/hotdog.png",
+  },
+  {
+    name: "ice-cream",
+    img: "images/ice-cream.png",
+  },
+  {
+    name: "milkshake",
+    img: "images/milkshake.png",
+  },
+  {
+    name: "pizza",
+    img: "images/pizza.png",
+  },
+];
+
+cardArry.sort(() => 0.5 - Math.random());
+console.log(cardArry);
+
+const gridDisplay = document.querySelector("#grid");
+
+const createBoard = () => {
+  for (let idx = 0; idx < cardArry.length; idx++) {
+    const card = document.createElement("img");
+    card.setAttribute("src", "images/blank.png");
+    card.setAttribute("data-id", idx);
+    card.addEventListener("click", flipCard);
+    gridDisplay.appendChild(card);
+  }
+};
+
+createBoard();
+
+function flipCard() {
+  this.console.log("flco");
+}
