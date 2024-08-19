3dfc9f178cf949c1046501399e9dbcaba01cb76f
senethmendis <mendis.seneth@gmail.com>
2024-08-19 21:08:00 +0530

    "Updated app.js to add result display and modified styles.css to add font and color styles"

--- memorygame/app.js
+++ memorygame/app.js
@@ -54,6 +54,7 @@ const cardArry = [
 
 cardArry.sort(() => 0.5 - Math.random());
 const gridDisplay = document.querySelector("#grid");
+const resultDisplay = document.querySelector("#result");
 
 const cardsWon = [];
 let cradsChoose = [];
@@ -61,15 +62,35 @@ let cradsChoosenIds = [];
 
 const checkMatch = () => {
   const cards = document.querySelectorAll("#grid img");
+  const optionOneId = cradsChoosenIds[0];
+  const optionTwoId = cradsChoosenIds[1];
   console.log("clicjk");
+  if (optionOneId == optionTwoId) {
+    alert("You have clicked the same img");
+  }
   if (cradsChoose[0] == cradsChoose[1]) {
     alert("found match");
-    cards[cradsChoosenIds[0]].setAttribute("src", "images/white.png");
-    cards[cradsChoosenIds[1]].setAttribute("src", "images/white.png");
-    cards[cradsChoosenIds[0]].removeEventListener("click", flipCard);
+
+    cards[optionOneId].setAttribute("src", "images/white.png");
+    cards[optionTwoId].setAttribute("src", "images/white.png");
+
+    cards[optionOneId].removeEventListener("click", flipCard);
+    cards[optionTwoId].removeEventListener("click", flipCard);
     cardsWon.push(cradsChoose);
+  } else {
+    cards[optionOneId].setAttribute("src", "images/blank.png");
+    cards[optionTwoId].setAttribute("src", "images/blank.png");
+
+    alert("sorry try again");
   }
+
+  resultDisplay.textContent = cardsWon.length;
+  cradsChoosenIds = [];
   cradsChoose = [];
+
+  if (cardsWon.length === cardArry.length / 2) {
+    resultDisplay.innerHTML = "Congrats you have found them all";
+  }
 };
 
 const createBoard = () => {
